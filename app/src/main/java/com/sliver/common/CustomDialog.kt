59ad0feee694c32bc825f473113d51b9c0351ce4
wzcxbg <39371565+wzcxbg@users.noreply.github.com>
2023-11-03 14:24:57 +0800

    Update CustomDialog.kt
    1. 解决设置padding参数无效的问题
    2. 为参数设置增加限制

--- app/src/main/java/com/sliver/common/CustomDialog.kt
+++ app/src/main/java/com/sliver/common/CustomDialog.kt
@@ -75,30 +75,31 @@ open class CustomDialog<T : ViewBinding>(context: Context) : Dialog(context) {
         fun customView(binding: T?) = apply { this.binding = binding }
         fun customView(clazz: Class<T>) = apply { this.binding = create(clazz) }
         fun applyBinding(listener: T.(Dialog) -> Unit) = apply { this.listener = listener }
-        fun backgroundGray(gray: Float) = apply { this.gray = gray }
-        fun width(width: Int) = apply { this.width = width }
-        fun height(height: Int) = apply { this.height = height }
-        fun gravity(gravity: Int) = apply { this.gravity = gravity }
-        fun animate(animateId: Int) = apply { this.anime = animateId }
-        fun cancelable(cancelAble: Boolean) = apply { this.cancelable = cancelAble }
-        fun padding(padding: Float) = apply { this.padding = RectF(padding, padding, padding, padding) }
-        fun padding(left: Float, top: Float, right: Float, bottom: Float) =
-            apply { this.padding = RectF(left, top, right, bottom) }
+        fun backgroundGray(@DimAmountRange gray: Float) = apply { this.gray = gray }
+        fun width(@LayoutParamFlags width: Int) = apply { this.width = width }
+        fun height(@LayoutParamFlags height: Int) = apply { this.height = height }
+        fun gravity(@GravityFlags gravity: Int) = apply { this.gravity = gravity }
+        fun animate(@AnimationFlags animateId: Int) = apply { this.anime = animateId }
+        fun cancelable(cancelable: Boolean) = apply { this.cancelable = cancelable }
+        fun padding(
+            @Dimension(unit = Dimension.DP) padding: Float
+        ) = apply { this.padding = RectF(padding, padding, padding, padding) }
+
+        fun padding(
+            @Dimension(unit = Dimension.DP) left: Float,
+            @Dimension(unit = Dimension.DP) top: Float,
+            @Dimension(unit = Dimension.DP) right: Float,
+            @Dimension(unit = Dimension.DP) bottom: Float
+        ) = apply { this.padding = RectF(left, top, right, bottom) }
 
         fun applyParameter(dialog: Dialog): Dialog {
             val window = dialog.window
             val decorView = window?.decorView
             val attributes = window?.attributes
 
-            val displayMetrics = context.resources.displayMetrics
-            decorView?.setPadding(
-                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, padding.left, displayMetrics).roundToInt(),
-                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, padding.top, displayMetrics).roundToInt(),
-                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, padding.right, displayMetrics).roundToInt(),
-                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, padding.bottom, displayMetrics).roundToInt(),
-            )
             window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            window?.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            //TODO 支持沉浸式状态栏
+            //window?.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
             window?.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
             window?.setWindowAnimations(anime)
             window?.setGravity(gravity)
@@ -110,6 +111,14 @@ open class CustomDialog<T : ViewBinding>(context: Context) : Dialog(context) {
             dialog.setCanceledOnTouchOutside(cancelable)
             dialog.setCancelable(cancelable)
             dialog.setContentView(view ?: binding?.root ?: return dialog)
+
+            val displayMetrics = context.resources.displayMetrics
+            decorView?.setPadding(
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, padding.left, displayMetrics).roundToInt(),
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, padding.top, displayMetrics).roundToInt(),
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, padding.right, displayMetrics).roundToInt(),
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, padding.bottom, displayMetrics).roundToInt(),
+            )
 
             listener?.invoke(binding ?: return dialog, dialog)
             return dialog
@@ -125,5 +134,64 @@ open class CustomDialog<T : ViewBinding>(context: Context) : Dialog(context) {
             val binding = inflate.invoke(inflate, LayoutInflater.from(context))
             return binding as T
         }
+
+        @Retention(AnnotationRetention.SOURCE)
+        @IntDef(
+            flag = false,
+            open = false,
+            value = [
+                Gravity.FILL,
+                Gravity.FILL_HORIZONTAL,
+                Gravity.FILL_VERTICAL,
+                Gravity.START,
+                Gravity.END,
+                Gravity.LEFT,
+                Gravity.RIGHT,
+                Gravity.TOP,
+                Gravity.BOTTOM,
+                Gravity.CENTER,
+                Gravity.CENTER_HORIZONTAL,
+                Gravity.CENTER_VERTICAL,
+                Gravity.DISPLAY_CLIP_HORIZONTAL,
+                Gravity.DISPLAY_CLIP_VERTICAL,
+                Gravity.CLIP_HORIZONTAL,
+                Gravity.CLIP_VERTICAL,
+                Gravity.NO_GRAVITY
+            ]
+        )
+        annotation class GravityFlags
+
+        @Retention(AnnotationRetention.SOURCE)
+        @IntDef(
+            flag = false,
+            open = true,
+            value = [
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
+            ]
+        )
+        annotation class LayoutParamFlags
+
+        @Retention(AnnotationRetention.SOURCE)
+        @IntDef(
+            flag = false,
+            open = true,
+            value = [
+                android.R.style.Animation_Dialog,
+                android.R.style.Animation_InputMethod,
+                android.R.style.Animation_Toast,
+                android.R.style.Animation_Translucent,
+            ]
+        )
+        annotation class AnimationFlags
+
+        @Retention(AnnotationRetention.SOURCE)
+        @FloatRange(
+            from = 0.0,
+            to = 1.0,
+            fromInclusive = true,
+            toInclusive = true,
+        )
+        annotation class DimAmountRange
     }
 }
