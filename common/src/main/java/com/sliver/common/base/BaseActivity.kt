3d91d58e033fa60a159ba094fec8d7b89010beee
Sliver <bohong.wu@emeet.ai>
2024-02-29 15:23:50 +0800

    1.基类BaseActivity、BaseFragment添加实用方法，兼容在启用混淆情况下的使用

--- common/src/main/java/com/sliver/common/base/BaseActivity.kt
+++ common/src/main/java/com/sliver/common/base/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.sliver.common.base
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
@@ -10,13 +11,9 @@ open class BaseActivity<T : ViewBinding> : AppCompatActivity() {
     protected val TAG = this::class.java.simpleName
     protected lateinit var binding: T
 
-    @Suppress("UNCHECKED_CAST")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val superClass = this.javaClass.genericSuperclass as ParameterizedType
-        val bindingClass = superClass.actualTypeArguments[0] as Class<*>
-        val method = bindingClass.getMethod("inflate", LayoutInflater::class.java)
-        binding = method.invoke(null, layoutInflater) as T
+        binding = createBinding(this)
         setContentView(binding.root)
         initData()
         initView()
@@ -26,4 +23,34 @@ open class BaseActivity<T : ViewBinding> : AppCompatActivity() {
     protected open fun initData() {}
     protected open fun initView() {}
     protected open fun initListener() {}
+
+    protected fun <T> argumentOf(name: String, default: T? = null): Lazy<T> {
+        @Suppress("UNCHECKED_CAST")
+        return lazy(LazyThreadSafetyMode.PUBLICATION) {
+            //NullPointerException -> IllegalStateException
+            intent?.extras ?: return@lazy default!!
+            val result = try {
+                intent.extras?.get(name) as T
+            } catch (e: ClassCastException) {
+                null
+            }
+            result ?: (default as T)
+        }
+    }
+
+    private fun createBinding(context: Context): T {
+        val superClass = this.javaClass.genericSuperclass as ParameterizedType
+        val bindingClass = superClass.actualTypeArguments[0] as Class<*>
+        val inflate = try {
+            bindingClass.getMethod("inflate", LayoutInflater::class.java)
+        } catch (e: Exception) {
+            bindingClass.declaredMethods.first {
+                val argumentTypes = arrayOf(LayoutInflater::class.java)
+                it.parameterTypes.contentEquals(argumentTypes)
+            }
+        }
+        inflate.isAccessible = true
+        @Suppress("UNCHECKED_CAST")
+        return inflate.invoke(null, LayoutInflater.from(context)) as T
+    }
 }
--- common/src/main/java/com/sliver/common/base/BaseFragment.kt
+++ common/src/main/java/com/sliver/common/base/BaseFragment.kt
@@ -12,12 +12,12 @@ open class BaseFragment<T : ViewBinding> : Fragment() {
     protected val TAG = this::class.java.simpleName
     protected open lateinit var binding: T
 
-    @Suppress("UNCHECKED_CAST")
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val superClass = this.javaClass.genericSuperclass as ParameterizedType
-        val bindingClass = superClass.actualTypeArguments[0] as Class<*>
-        val method = bindingClass.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
-        binding = method.invoke(null, inflater, container, false) as T
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = createBinding(inflater, container)
         return binding.root
     }
 
@@ -31,4 +31,47 @@ open class BaseFragment<T : ViewBinding> : Fragment() {
     protected open fun initData() {}
     protected open fun initView() {}
     protected open fun initListener() {}
+
+    protected fun <T> argumentOf(name: String, default: T? = null): Lazy<T> {
+        @Suppress("UNCHECKED_CAST")
+        return lazy(LazyThreadSafetyMode.PUBLICATION) {
+            //NullPointerException -> IllegalStateException
+            arguments ?: return@lazy default!!
+            val result = try {
+                arguments?.get(name) as T
+            } catch (e: ClassCastException) {
+                null
+            }
+            result ?: (default as T)
+        }
+    }
+
+    private fun createBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        attachToRoot: Boolean = false,
+    ): T {
+        val superClass = this.javaClass.genericSuperclass as ParameterizedType
+        val bindingClass = superClass.actualTypeArguments[0] as Class<*>
+        val inflate = try {
+            bindingClass.getMethod(
+                "inflate",
+                LayoutInflater::class.java,
+                ViewGroup::class.java,
+                Boolean::class.java
+            )
+        } catch (e: Exception) {
+            bindingClass.declaredMethods.first {
+                val argumentTypes = arrayOf(
+                    LayoutInflater::class.java,
+                    ViewGroup::class.java,
+                    Boolean::class.java
+                )
+                it.parameterTypes.contentEquals(argumentTypes)
+            }
+        }
+        inflate.isAccessible = true
+        @Suppress("UNCHECKED_CAST")
+        return inflate.invoke(null, inflater, container, attachToRoot) as T
+    }
 }
